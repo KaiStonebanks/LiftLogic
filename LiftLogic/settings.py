617db4b1add944ab7ed75c89065ae2fcad31d11e
“Efe <2847709d@student.gla.ac.uk>
2026-03-16 20:50:01 +0000

    With Background

--- LiftLogic/settings.py
+++ LiftLogic/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
